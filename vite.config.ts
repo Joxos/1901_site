e9e845da7851ac41f09dbbf78654e28c13bd8fbf
Joxos <johndoe@example.com>
2022-09-10 10:43:37 +0800

    add preset-attributify

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 import presetWind from '@unocss/preset-wind'
+import presetAttributify from '@unocss/preset-attributify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Unocss({
-      presets: [presetWind()],
+      presets: [presetWind(), presetAttributify({})],
     }),
   ],
   resolve: {
