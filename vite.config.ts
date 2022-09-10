5abe0b5b7862a82a200350534885e8f6fe02c68a
Joxos <johndoe@example.com>
2022-09-10 10:35:21 +0800

    add preset-wind

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
+import presetWind from '@unocss/preset-wind'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss({})],
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [presetWind()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
