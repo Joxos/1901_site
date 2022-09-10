3db4ffc9290d08c756fa7e290f72b196c4511c2b
Joxos <johndoe@example.com>
2022-09-10 11:57:41 +0800

    add preset-web-fonts

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,39 @@ import Unocss from 'unocss/vite'
 import presetWind from '@unocss/preset-wind'
 import presetAttributify from '@unocss/preset-attributify'
 import presetIcons from '@unocss/preset-icons'
+import presetWebFonts from '@unocss/preset-web-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Unocss({
-      presets: [presetWind(), presetAttributify({}), presetIcons({})],
+      presets: [
+        presetWind(),
+        presetAttributify({}),
+        presetIcons({}),
+        presetWebFonts({
+          provider: 'google', // default provider
+          fonts: {
+            // these will extend the default theme
+            sans: 'Roboto',
+            mono: ['Fira Code', 'Fira Mono:400,700'],
+            // custom ones
+            lobster: 'Lobster',
+            lato: [
+              {
+                name: 'Lato',
+                weights: ['400', '700'],
+                italic: true,
+              },
+              {
+                name: 'sans-serif',
+                provider: 'none',
+              },
+            ],
+          },
+        }),
+      ],
     }),
   ],
   resolve: {
